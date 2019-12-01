3cfb01e017a1f90d38305fc6f72672142cfcf5d5
Nguyễn Mạnh Cường <nguyenmanhcuong03@vccorp.vn>
2019-12-01 14:51:10 +0700

    Update source files, apply pre-commit hook

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,7 +11,7 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js')
+mix.copy('resources/js/app.js', 'public/js')
   .copy('resources/bower_components/jquery/dist/jquery.js', 'public/js')
   .copy('resources/semantic', 'public/semantic')
   .sass('resources/sass/main.scss', 'public/css');
